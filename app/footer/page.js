93f83ac3cfda66cf7ddf6fe30d48f2de55448731
tomadadefogo <pedrobkluiz@hotmail.com>
2023-08-15 17:41:53 -0300

    Home completa

--- app/footer/page.js
+++ app/footer/page.js
@@ -8,11 +8,12 @@ export default function Footer() {
 
   return (
     <>
-      <footer class='flex  flex-wrap bg-slate-300 mt-6'>
-  <div className="flex mt-12">
+      <footer class='flex  flex-wrap bg-slate-300 mt-6 max-[640px]:block max-[640px]:mt-32'>
+  <div className="flex mt-12 max-[640px]:block max-[640px]:mt-32">
 
-    <div class="ml-12 shadow-xl">
+    <div class="ml-12 shadow-xl max-[640px]:mt-12">
       <iframe
+      
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2599.431845100454!2d-43.25921631649451!3d-22.870851277726107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd88dcb13aa6d6d5%3A0xcba3f5a967431107!2sRua%20Humboldt%20153%2C%20Bonsucesso%2C%20RJ%20-%20LOJA%20VIRTUAL%20ONLINE%2C%20%22BAZAR%20%26%20CIA%20-%20DAVI%20SILVA%22.%20XEROX%2C%20JUKEBOX%20E%20KARAOK%C3%8A.!5e0!3m2!1spt-BR!2sbr!4v1691884795855!5m2!1spt-BR!2sbr"
         width="450"
         height="250"
@@ -20,13 +21,14 @@ export default function Footer() {
         allowFullScreen=""
         loading="lazy"
         title="Google Map"
+        class="max-[640px]:w-[300px] max-[640px]:pt-6 max-[640px]:mt-12 "
       ></iframe>
     </div>
     
    
-    <div class="border-l border-red-500 ml-6"></div>
+    <div class="border-l border-red-500 ml-6 max-[640px]:block"></div>
     <div>
-      <h3 class='ml-6 max-w-sm'>R. Humboldt, 153 - Bonsucesso, Rio de Janeiro - RJ, 21050-140   
+      <h3 class='ml-6 max-w-sm max-[640px]:ml-0'>R. Humboldt, 153 - Bonsucesso, Rio de Janeiro - RJ, 21050-140   
         <span class={`mt-8 ml-4 rounded-xl px-4 ${isOpen ? 'bg-green-500' : 'bg-red-500'}`}>
                 {isOpen ? 'Aberto' : 'Fechado'}
         </span> 
@@ -40,14 +42,18 @@ export default function Footer() {
       </ul>
     </div>
 
-    <div class="border-l border-red-500"></div>
-    <div class='ml-12 shadow-sm'>
+    <div class="border-l border-red-500 max-[640px]:inline-block"></div>
+    <div class='ml-12 shadow-sm max-[640px]:flex max-[640px]:my-0'>
       <Link href=''><Image src='/facebook.png' alt='facebook logo' width={40} height={40} /></Link>
-      <Link href=''><Image class='my-2' src='/twitter.png' alt='Twitter logo' width={40} height={40} /></Link>
+      <div class='max-[640px]:my-0 max-[640px]:mx-2'>
+        <Link href=''><Image class='my-2 max-[640px]:m-0 max-[640px]:mx-2' src='/twitter.png' alt='Twitter logo' width={40} height={40} />
+        </Link>
+      </div>
+
       <Link href=''><Image src='/instagram.png' alt='Instagram logo' width={40} height={40} /></Link>
     </div>
 
-    <div class=' ml-4'>
+    <div class=' ml-4 max-[640px]:block'>
       <h3 class='text-red-500'>Informe-se</h3>
       <Link class='' href=''>Nossos Funcionários</Link>
     </div>
--- app/page.js
+++ app/page.js
@@ -3,17 +3,20 @@
 import Modal from './modal/page'
 import Link from 'next/link'
 import Image from 'next/image'
-
+import Head from 'next/head'
 export default function page() {
 
   
 
   return (
     <>
+    <Head>
+        <link rel="icon" href="/icon.ico" />
+        </Head>
      <Modal/>
    
-    <section id='homeConfig' className='relative mt-12'>
-  <div  className="relative">
+    <section className='relative mt-12'>
+  <div  className="relative sm:text-left">
     <Image
       className="bg-cover w-screen"
       src='/homefoto.jpeg'
@@ -23,11 +26,11 @@ export default function page() {
     />
     <div className="absolute inset-0 bg-black opacity-50"></div>
       <div className="absolute top-0 right-0 p-4 z-10">
-        <h2 className="text-white text-3xl text-center max-w-[400px] mt-12 mr-36 shadow-md">
+        <h2 className="text-white text-3xl text-center mr-24 max-w-[400px] mt-12 max-[640px]:text-sm max-[640px]:mr-0 max=[640]:mt-0">
         Uma doação é uma ação poderosa que pode fazer uma grande diferença em uma ONG que cuida de crianças. Ao contribuir financeiramente, você está oferecendo suporte direto para a missão da organização, que pode abranger uma ampla gama de serviços e benefícios para as crianças em necessidade.
         </h2>
       </div>
-        </div>
+  </div>
 </section>
 <section>
   <h2 class='text-center text-3xl text-white mt-12'>
@@ -36,30 +39,32 @@ export default function page() {
   <div class='border border-white mt-6 max-w-[400px] mx-auto'></div>
 </section>
 <section id='fotoConfig' class='mt-12'>
-  <div class="flex justify-between mx-12">
-    <Link href='' class="relative">
+  <div class="flex justify-between mx-12 max-[640px]:block "> 
+    <Link href='' class="relative max-[640]">
       <Image
         src="/homefoto.jpeg"
         alt="Foto 1"
         width={300}
         height={500}
       />
-      <h3 class="absolute bottom-0 left-0 p-2 bg-black bg-opacity-50 text-white hover:bg-red-500">
+      <h3 class="absolute bottom-0 left-0 p-2 bg-black bg-opacity-50 text-white hover:bg-red-500 ">
         Evento com parceria da SEBRAE atrai centenas de crianças
       </h3>
     </Link>
-    <Link href='' class="relative">
-      <Image
-        src="/homefoto.jpeg"
-        alt="Foto 2"
-        width={300}
-        height={500}
-      />
-      <h3 class="absolute bottom-0 left-0 p-2 bg-black bg-opacity-50 text-white hover:bg-red-500">
-        Evento com parceria da SEBRAE atrai centenas de crianças
-      </h3>
-    </Link>
-    <Link href='' class="relative">
+    <div class="max-[640px]:my-6">
+      <Link href='' class="relative max-[640]: my-6  ">
+        <Image
+          src="/homefoto.jpeg"
+          alt="Foto 2"
+          width={300}
+          height={500}
+        />
+        <h3 class="absolute bottom-0 left-0 p-2 bg-black bg-opacity-50 text-white hover:bg-red-500">
+          Evento com parceria da SEBRAE atrai centenas de crianças
+        </h3>
+      </Link>
+    </div>
+    <Link href='' class="relative max-[640]">
       <Image
         src="/homefoto.jpeg"
         alt="Foto 3"
@@ -72,7 +77,9 @@ export default function page() {
     </Link>
   </div>
 </section>
-<section>
+
+
+<section class='max-[640px]:mb-12'>
   <div class='flex justify-between mx-12'>
     <div class="w-1/3 relative">
       <div class='border border-white mt-6'></div>
